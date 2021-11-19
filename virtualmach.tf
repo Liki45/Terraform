04136a602cdf975d6dc4711b258ad06bc36984df
Liki45 <93913653+Liki45@users.noreply.github.com>
2021-11-19 19:32:35 +0530

    Create virtualmach.tf

---
+++ virtualmach.tf
@@ -0,0 +1,40 @@
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.VirtualMachineName
+  resource_group_name = azurerm_resource_group.MyResourceGroup.name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.MyTerraformNIC.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh-key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name=var.VirtualMachineName
+  disable_password_authetication=true
+}
+
+resource "tls_private_key" "ssh-key"{
+  algorithm   = "RSA"
+  rsa_bits =4096
+}
+
+output "tls_private_key"{
+ value=tls_private_key.ssh-key.private_key_pem
+  sensitive=true
+}
